9c84b60c7024540a6d45c03402701cceaa2a0b4f
amr-ahmed1 <amr.ahmed1@ibm.com>
2021-07-09 01:35:22 +0200

    Add moveForward signature

--- src/rover/rover.ts
+++ src/rover/rover.ts
@@ -4,4 +4,8 @@ import { RoverDirection } from './directions';
 export class Rover {
   coordinates: Coordinates = new Coordinates(4, 2);
   direction: RoverDirection = RoverDirection.NORTH;
+
+  moveForward() {
+    throw new Error('Method not implemented.');
+  }
 }
