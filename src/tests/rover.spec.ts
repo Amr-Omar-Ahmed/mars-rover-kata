bcff61bd480910a8655921bf87b2aa03dc03616e
amr-ahmed1 <amr.ahmed1@ibm.com>
2021-07-09 18:23:31 +0200

    Add test cases for all commands at EAST

--- src/tests/rover.spec.ts
+++ src/tests/rover.spec.ts
@@ -37,6 +37,33 @@ describe('Rover with initial position is (4, 2) NORTH', () => {
         rover.excuteCommands('L');
         expect(rover.position.getRoverPosition()).to.equal(`(4,2) ${RoverHeadings.WEAST}`);
       });
+
+      it('Should execute multiple commands', () => {
+        rover.excuteCommands('FFFFBBBBR');
+        expect(rover.position.getRoverPosition()).to.equal(`(4,2) ${RoverHeadings.EAST}`);
+      });
+    });
+
+    describe('EAST Heading', () => {
+      it('Should move forward four times with commands FFFF', () => {
+        rover.excuteCommands('FFFF');
+        expect(rover.position.getRoverPosition()).to.equal(`(8,2) ${RoverHeadings.EAST}`);
+      });
+
+      it('Should move backward four times with commands BBBB', () => {
+        rover.excuteCommands('BBBB');
+        expect(rover.position.getRoverPosition()).to.equal(`(0,2) ${RoverHeadings.EAST}`);
+      });
+
+      it('Should rotate right, and heading becomes at EAST', () => {
+        rover.excuteCommands('R');
+        expect(rover.position.getRoverPosition()).to.equal(`(4,2) ${RoverHeadings.WEAST}`);
+      });
+
+      it('Should rotate left, and heading becomes at NORTH', () => {
+        rover.excuteCommands('L');
+        expect(rover.position.getRoverPosition()).to.equal(`(4,2) ${RoverHeadings.NORTH}`);
+      });
     });
   });
 });
