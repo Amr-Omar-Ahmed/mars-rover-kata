6803abf66b9e60ae99022cbc678ee6c60ce18174
amr-ahmed1 <amr.ahmed1@ibm.com>
2021-07-09 06:26:56 +0200

    Adding Postion class with more configurations for commands

---
+++ src/rover/position.ts
@@ -0,0 +1,23 @@
+import { RoverDirection } from './directions';
+
+export class Position {
+  x = 4;
+  y = 2;
+  direction: RoverDirection = RoverDirection.NORTH;
+
+  getRoverPosition(): string {
+    return `(${this.x},${this.y}) ${this.direction}`;
+  }
+}
+
+export const HEADING_AVAILABLE_COMMANDS: { [key: string]: Commands } = {
+  NORTH: {
+    F: (position: Position) => {
+      position.y++;
+    },
+  },
+};
+
+export interface Commands {
+  F: (position: Position) => void;
+}
