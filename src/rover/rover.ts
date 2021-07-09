0107d16ae7393a9f6f72d195749ef4bed4eb55b4
amr-ahmed1 <amr.ahmed1@ibm.com>
2021-07-09 20:22:50 +0200

    Pass test cases for entering obstacles

--- src/rover/rover.ts
+++ src/rover/rover.ts
@@ -5,9 +5,22 @@ export class Rover {
   space: Space = new Space();
 
   excuteCommands(commands: string) {
+    let isRoverCanBeDamaged = false;
     commands.split('').forEach((command: 'F' | 'R' | 'L' | 'B') => {
+      if (isRoverCanBeDamaged) {
+        return;
+      }
+      const prevX = this.space.position.x;
+      const prevY = this.space.position.y;
       const currentHeading = this.space.position.heading;
       HEADING_AVAILABLE_COMMANDS[currentHeading][command](this.space.position);
+      isRoverCanBeDamaged = this.space.obstacles.some(
+        (obs) => obs.x === this.space.position.x && obs.y === this.space.position.y
+      );
+      if (isRoverCanBeDamaged) {
+        this.space.position.x = prevX;
+        this.space.position.y = prevY;
+      }
     });
   }
 }
