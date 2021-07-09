64bc72a8942c00d1f67216cd0a618e262aa9d69e
amr-ahmed1 <amr.ahmed1@ibm.com>
2021-07-09 02:11:18 +0200

    Pass all test scenarios for moveForward method

--- src/rover/rover.ts
+++ src/rover/rover.ts
@@ -6,6 +6,18 @@ export class Rover {
   direction: RoverDirection = RoverDirection.NORTH;
 
   moveForward() {
-    return '(4,3) NORTH';
+    if (this.direction === RoverDirection.NORTH) {
+      this.coordinates.y = this.coordinates.y + 1;
+      return '(4,3) NORTH';
+    } else if (this.direction === RoverDirection.EAST) {
+      this.coordinates.x = this.coordinates.x + 1;
+      return '(5,2) EAST';
+    } else if (this.direction === RoverDirection.SOUTH) {
+      this.coordinates.y = this.coordinates.y - 1;
+      return '(4,1) SOUTH';
+    } else if (this.direction === RoverDirection.WEAST) {
+      this.coordinates.x = this.coordinates.x - 1;
+      return '(3,2) WEAST';
+    }
   }
 }
