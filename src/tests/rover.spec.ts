99f79795eac78bd4b8c31440cb169ae0592fa95e
amr-ahmed1 <amr.ahmed1@ibm.com>
2021-07-09 02:30:37 +0200

    Refactor rover test cases

--- src/tests/rover.spec.ts
+++ src/tests/rover.spec.ts
@@ -4,6 +4,7 @@ import { Rover } from '../rover/rover';
 
 describe('Rover with initial position is (4, 2) NORTH', () => {
   let rover: Rover;
+  let initialPosition = '(4,2) NORTH';
   beforeEach(() => {
     rover = new Rover();
   });
@@ -11,33 +12,33 @@ describe('Rover with initial position is (4, 2) NORTH', () => {
   describe('Rover can move forward', () => {
     it('When rover heading is at North, y coordinate should be increment with one', () => {
       rover.direction = RoverDirection.NORTH;
-      const result = rover.moveForward();
-      expect(result).to.equal('(4,3) NORTH');
+      const currentPosition = rover.moveForward();
+      expect(currentPosition).to.equal(rover.getRoverPosition());
     });
 
     it('When rover heading is at East, x coordinate should be increment with one', () => {
       rover.direction = RoverDirection.EAST;
-      const result = rover.moveForward();
-      expect(result).to.equal('(5,2) EAST');
+      const currentPosition = rover.moveForward();
+      expect(currentPosition).to.equal(rover.getRoverPosition());
     });
 
     it('When rover heading is at South, y coordinate should be decrement by one', () => {
       rover.direction = RoverDirection.SOUTH;
-      const result = rover.moveForward();
-      expect(result).to.equal('(4,1) SOUTH');
+      const currentPosition = rover.moveForward();
+      expect(currentPosition).to.equal(rover.getRoverPosition());
     });
 
     it('When rover heading is at Weast, x coordinate should be decrement by one', () => {
       rover.direction = RoverDirection.WEAST;
-      const result = rover.moveForward();
-      expect(result).to.equal('(3,2) WEAST');
+      const currentPosition = rover.moveForward();
+      expect(currentPosition).to.equal(rover.getRoverPosition());
     });
   });
 
-  describe('Get Rover Position', () => {
+  describe('Get Rover Initial Position', () => {
     it('Get rover coordinates and direction', () => {
       const position = rover.getRoverPosition();
-      expect(position).to.equal('(4,2) NORTH');
+      expect(position).to.equal(initialPosition);
     });
   });
 });
